eaa1187c86243a8bee4094022485ec4d5387a2d2
RSouzaRibeiro <rafaelsouza803@gmail.com>
2020-03-25 23:47:19 -0300

    Teste mudanca de banco

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express();
  * {origin: "url que pode acessar a aplicaçao"}
  */
 app.use(cors({
-    origin: "https://bethehero-frontend.herokuapp.com/"
+    origin: "https://bethehero-frontend.herokuapp.com"
 }));
 app.use(express.json());
 app.use(routes);
