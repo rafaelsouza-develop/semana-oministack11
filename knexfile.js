567ad509e297304659dee0465d85c118d7780c3b
RSouzaRibeiro <rafaelsouza803@gmail.com>
2020-03-25 22:35:30 -0300

    Changed DB

--- knexfile.js
+++ knexfile.js
@@ -32,16 +32,17 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: "ec2-54-157-78-113.compute-1.amazonaws.com",
+      database: 'd4l651v3qth05',
+      user:     'pyodgpouohzmta',
+      password: 'b0ba3695d5456c1feb6beeafc261ec4c454a25ee013992cfe25cd1f86341a228'
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './src/database/migrations'
     }
   }
 
