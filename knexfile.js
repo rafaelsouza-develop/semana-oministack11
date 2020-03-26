2b41b8a5190b26505bab90a5efa0a19cca4c953a
RSouzaRibeiro <rafaelsouza803@gmail.com>
2020-03-26 01:22:24 -0300

    Config base url to Heroku

--- knexfile.js
+++ knexfile.js
@@ -32,10 +32,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: './src/database/migrations'
     }
